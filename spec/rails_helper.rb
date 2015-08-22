403a13abb04efebf3dafe06e986bb0ace6d603ef
Bair Bolsoboev <bairbol@gmail.com>
2015-08-22 13:46:38 +0800

    Finish static pages

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,9 +3,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'spec_helper'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -50,4 +50,5 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.include Capybara::DSL
 end
